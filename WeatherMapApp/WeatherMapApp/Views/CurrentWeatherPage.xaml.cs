b78884c134eea40ae4833eaf6fe5d922318e211d
Danylo Labenskyi <danylolab@gmail.com>
2018-03-14 16:45:14 +0200

    Created empty CurrentWeatherPage

---
+++ WeatherMapApp/WeatherMapApp/Views/CurrentWeatherPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WeatherMapApp.Views
+{
+    public partial class CurrentWeatherPage : ContentPage
+    {
+        public CurrentWeatherPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
